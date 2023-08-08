61febf6add4211af73708e5f34d9bb7b4e834a5d
harleydi <disneyharley26@gmail.com>
2023-08-08 10:58:05 -0400

    added controller and model for Orders

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,10 +1,28 @@
+const Dock = require('../models/Dock')
 const Order = require('../models/Order')
+
+// be sure to push orders to user
 
 const createOrder = async (req, res) => {
     try {
-        const newOrder = await new Order
-        await newOrder.save()
+        const body = req.body
+        const newOrder = await new Order(body)
+        const savedOrder = await newOrder.save()
+        const updateDock = await Dock.findOneAndUpdate({ _id: savedOrder.orderDock }, {
+            $push: { orders: savedOrder._id }
+        })
         res.status(200).json({ success: true, data: newOrder})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ success: false, message: "error", error: error })
+    }
+}
+
+const deleteOrder = async (req, res) => {
+    try {
+        const { id } = useParams()
+        const deletedOrder = await Order.findOneAndDelete({ _id: id })
+        res.status(200).json({ success: true, data: deletedOrder })
     } catch (error) {
         console.log(error)
         res.status(500).json({ success: false, message: "error", error: error })
--- models/Order.js
+++ models/Order.js
@@ -4,10 +4,10 @@ const { v4 : uuid } = require('uuid')
 const orderSchema = new mongoose.Schema({
     _id: { type: String, default: uuid },
     orderOwner: { type: String, ref: "user" },
-    orderTotal: { type: Number, default: 0.00 },
+    orderTotal: { type: Number },
     orderItems: [{ type: Object, ref: "product" }],
-    orderDock: { type: String, default: "Staging Dock" },
-    orderStatus: { type: { enum: ['order recieved', 'order being prepared', 'order ready for pick', 'order complete']}}
+    orderDock: { type: String, ref: "dock", default: "1547169e-52f6-4a33-a5c7-758af56caa5f" }, // Should I make this to enum? How can i make this to keep up with active docks?
+    orderStatus: { type: String,  enum: ['order recieved', 'order being prepared', 'order ready for pick', 'order complete'], default: 'order recieved'}
 })
 
 const Order = mongoose.model("order", orderSchema)
